c84ca6fc2a7e063348e428d42526640c9bd17bdd
VANLERBERGHE Michael <v19009228@A-F002474.salsa.univ-amu.fr>
2020-02-28 09:13:32 +0100

    classe Partition

---
+++ src/Musiciens/MusicienFactory.java
@@ -0,0 +1,21 @@
+package Musiciens;
+
+public class MusicienFactory {
+	
+	//créer un musicien avec son nom en fonction de l'instrument passé en paramètre, si l'instrument n'existe pas renvoie null
+	static Musicien Build(String instrument, String nom) {
+		Musicien musicien = null;
+		switch(instrument) {
+			case "Piano":
+				musicien = new MusicienPiano(nom);
+			break;
+			case "Violin":
+				musicien = new MusicienViolon(nom);
+			break;
+			case "Trumpet":
+				musicien = new MusicienTrompette(nom);
+			break;
+		}
+		return musicien;
+	}
+}
