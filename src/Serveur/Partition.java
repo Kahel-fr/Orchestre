70365e0d56f34dc8fd37b6cdc81d55e6087c6e26
VANLERBERGHE Michael <v19009228@A-F002474.salsa.univ-amu.fr>
2020-02-28 09:31:26 +0100

    Notification des musiciens lors de l'ajout d'une partition

---
+++ src/Serveur/Partition.java
@@ -0,0 +1,20 @@
+//représente une partition de musique
+public class Partition {
+	
+	String nom;
+	char Tonalite;
+	
+	public Partition(String nom, char tonalite) {
+		super();
+		this.nom = nom;
+		Tonalite = tonalite;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public char getTonalite() {
+		return Tonalite;
+	}
+}
